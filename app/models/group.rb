df376c7a94b5a23ce5098e5a7aaef2c87d5af66d
mali <mailfullstack@gmail.com>
2017-01-01 18:09:08 +0800

    implement is_member_of?(group)

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,4 +5,7 @@ class Group < ActiveRecord::Base
   has_many :group_relationships
   has_many :members, through: :group_relationships, source: :user
 
+  def is_memeber_of?(group)
+    participated_groups.include?(group)
+  end
 end
